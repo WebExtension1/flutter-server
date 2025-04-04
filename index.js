1a0a3d9556de8cfd0640de6e4416f01dd46857a8
Robert Jenner <85253281+WebExtension1@users.noreply.github.com>
2025-04-04 18:43:03 +0100

    Server

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import { createServer } from "http";
+import { Server } from "socket.io";
 
 // Router imports
 import postRouter from "./routes/post.js";
 import commentRouter from "./routes/comment.js";
 import accountRouter from "./routes/account.js";
 
+// Configs
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: "*" }));
+
+// Server
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("Connected to server");
+
+  socket.on("chat message", (data) => {
+    console.log(data);
+    io.emit("chat message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Disconnected from server");
+  });
+});
 
 // Routes
 app.use("/post", postRouter);
@@ -22,4 +47,6 @@ app.use((err, res) => {
   res.status(500).json({ error: "Internal Server Error" });
 });
 
-app.listen(3005, () => console.log("Backend API running on port 3005"));
+app.listen(3005, () => console.log("API running on port 3005"));
+
+server.listen(3006, () => console.log("Server running on port 3006"));
